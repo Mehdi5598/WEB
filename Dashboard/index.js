cd72a34253bb59a1096577a757a0a00db1dcc6fb
Mehdi5598 <118064049+Mehdi5598@users.noreply.github.com>
2024-03-17 22:16:57 +0100

    Add files via upload

---
+++ Dashboard/index.js
@@ -0,0 +1,11 @@
+let sideMenu =  document.querySelector("aside");
+let menuBtn = document.querySelector("#menu-btn");
+let closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click',() => {
+    sideMenu.style.display = 'block' ; 
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
